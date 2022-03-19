1442d333dffb185eae309c285a985aeaf385ed41
Aditya Meena <adityameena112@gmail.com>
2022-03-19 18:35:37 +0530

    Initial version of jhipster generated by generator-jhipster@7.7.0

---
+++ src/main/java/com/cms/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.cms.repository;
+
+import com.cms.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/cms/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.cms.web.rest.vm;
